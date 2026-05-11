36e1f2139e1ed31f07fb5d448c1102d2115acb9e
Picardas <167648023+Picardas@users.noreply.github.com>
2026-05-11 23:21:31 +0100

    Manual bump to v2.8.0

--- Casks/gcm/picardas-git-credential-manager.rb
+++ Casks/gcm/picardas-git-credential-manager.rb
@@ -1,9 +1,14 @@
 cask "picardas-git-credential-manager" do
   arch arm: "arm64", intel: "x64"
 
-  version "2.7.3"
-  sha256 arm:   "7051a5e953bcd37a182330a0f18bc6842c4cac143b537f8c49a8c5906d335c80",
-         intel: "f172be74f3c8fb38c69a93464d6c07a911f2259a62304d0adc60142f8d2c9226"
+  version "2.8.0"
+
+  on arm do
+    shas256 "687ab927a4e2aa2e3298a777c0185f6e1b85bef0e77e7010fb0f6929da2c8b4e"
+  end
+  on intel do
+    sha256 "c304c5d704b35255c8b8a6e1d45cf3c58d1daaf08c73b3f8503eab53c45c6498"
+  end
 
   url "https://github.com/git-ecosystem/git-credential-manager/releases/download/v#{version.major_minor_patch}/gcm-osx-#{arch}-#{version.major_minor_patch}.tar.gz",
       verified: "github.com/git-ecosystem/git-credential-manager/"
