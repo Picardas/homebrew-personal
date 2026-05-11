c758966b7aa03468f1d74c74f38ed15f9a0f3b45
Picardas <167648023+Picardas@users.noreply.github.com>
2026-05-11 23:39:53 +0100

    Manual bump to v2.8.0, removed Intel arch

--- Casks/gcm/picardas-git-credential-manager.rb
+++ Casks/gcm/picardas-git-credential-manager.rb
@@ -1,23 +1,16 @@
 cask "picardas-git-credential-manager" do
-  arch arm: "arm64", intel: "x64"
-
   version "2.8.0"
+  sha256 "687ab927a4e2aa2e3298a777c0185f6e1b85bef0e77e7010fb0f6929da2c8b4e"
 
-  on arm do
-    shas256 "687ab927a4e2aa2e3298a777c0185f6e1b85bef0e77e7010fb0f6929da2c8b4e"
-  end
-  on intel do
-    sha256 "c304c5d704b35255c8b8a6e1d45cf3c58d1daaf08c73b3f8503eab53c45c6498"
-  end
-
-  url "https://github.com/git-ecosystem/git-credential-manager/releases/download/v#{version.major_minor_patch}/gcm-osx-#{arch}-#{version.major_minor_patch}.tar.gz",
+  url "https://github.com/git-ecosystem/git-credential-manager/releases/download/v#{version.major_minor_patch}/gcm-osx-arm64-#{version.major_minor_patch}.tar.gz",
       verified: "github.com/git-ecosystem/git-credential-manager/"
   name "Git Credential Manager"
   desc "Cross-platform Git credential storage for multiple hosting providers"
+
   homepage "https://aka.ms/gcm"
 
   livecheck do
-    url "https://github.com/git-ecosystem/git-credential-manager/releases"
+    url :url
     strategy :github_latest
   end
 
